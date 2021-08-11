e264556cb7a3c372a174c928d03d7e0454edc20d
Sumitra <sumitrac2008@gmail.com>
2021-08-10 19:19:12 -0700

    components: Application, PasswordReset, ProfilePage, SignIn, SignUp

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@
 // import * as firebase from 'firebase';
 import 'firebase/firestore';
 // import './App.css';
-import { AuthProvider } from './components/auth/Auth';
-import Login from "./components/auth/Login";
-import Welcome from './Welcome';
+// import { AuthProvider } from './components/auth/Auth';
+// import Login from "./components/auth/Login";
+// import Welcome from './Welcome';
 import Footer from "./components/footer/Footer"
 
 // import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/navBar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Router } from 'react-router-dom';
 
 import About from './components/pages/About';
 import EventPage from './components/pages/EventPage';
@@ -24,15 +24,26 @@ import EventPage from './components/pages/EventPage';
 // import SignIn from './components/auth/SignIn'
 // import SignUp from './components/auth/SignUp'
 // import CreateEvent from './components/event/CreateEvent'
-
+import React, { useContext } from "react";
+// import { Router } from "@reach/router";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Application from "./components/Application";
+import UserProvider from "./providers/UserProvider";
+import ProfilePage from "./components/ProfilePage";
+import { UserContext } from "./providers/UserProvider";
 
 function App() {
     return (
       <div>
-        <AuthProvider>
+        {/* This is disable for now */}
+        {/* <AuthProvider>
           <Welcome />
-          <Login />
-        </AuthProvider> 
+          {/* <Login /> */}
+        {/* </AuthProvider>  */} 
+        <UserProvider>
+          <Application />
+        </UserProvider>
           
         <Router>
           <Navbar />
